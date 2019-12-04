b4f67baf8a8bb736c7a14ff7e80757a99c3efbc2
ga82bos <max.zitzmann@tum.de>
2019-12-04 20:17:34 +0100

    Solution day3_part2

--- 2019/day4/main.go
+++ 2019/day4/main.go
@@ -6,6 +6,13 @@ func main() {
 	start := 172_851
 	end := 675_869
 
+	//test := []int{112_222, 123_456, 111_122, 112_233, 223_333, 122_333, 111_111, 111_112, 111_122, 111_333}
+	//
+	//for _, i := range test {
+	//	fmt.Printf("%d: %t\n", i, meetsCriteria(i))
+	//}
+
+	//return
 	count := 0
 	for i := start; i <= end; i++ {
 		if meetsCriteria(i) {
@@ -16,21 +23,42 @@ func main() {
 }
 
 func meetsCriteria(num int) bool {
-	hasAdjacentDigits := false
-	isSorted := true
+	hasTuple := false
+	possibleTuple := false
+
+	inAdjacentGroup := false
 
 	digits := digits(num)
 	// note: iterate until i < len(digits)-1 because we access last element with i+1
 	for i := 0; i < len(digits)-1; i++ {
 		if digits[i] > digits[i+1] {
-			isSorted = false
-			break
+			return false
 		}
-		if digits[i] == digits[i+1] {
-			hasAdjacentDigits = true
-		}
+		match := digits[i] == digits[i+1]
+		possibleTuple = !match && possibleTuple || match && !inAdjacentGroup
+		hasTuple = !match && (hasTuple || inAdjacentGroup && possibleTuple) || match && hasTuple
+		inAdjacentGroup = match
+
+		/*
+			// more readable version below ;) Propositional calculus ftw
+			if digits[i] == digits[i+1] {
+				possibleTuple = !atAdjacentStart
+				atAdjacentStart = true
+
+				if !atAdjacentStart {
+					atAdjacentStart = true
+					possibleTuple = true
+					continue
+				}
+				possibleTuple = false
+			} else {
+				hasTuple = hasTuple || atAdjacentStart && possibleTuple
+				atAdjacentStart = false
+			}
+		*/
 	}
-	return hasAdjacentDigits && isSorted
+	hasTuple = hasTuple || inAdjacentGroup && possibleTuple // edge case for last 2 digits are tuple
+	return hasTuple
 }
 
 // digits takes a number and returns a slice of its digits, e.g. 123_456 => [1 2 3 4 5 6]
