e65c2c8eee327f3de8b94c5417186e8810650a80
ga82bos <max.zitzmann@tum.de>
2019-12-08 13:58:31 +0100

    Solution day6 p1

--- 2019/day6/main.go
+++ 2019/day6/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"gitea.curunir/Pokerkoffer/gitealoggertest/log"
 	"io/ioutil"
+	"math"
 	"os"
 	"strings"
 )
 
 var (
-	orbits map[string]string
+	orbits    map[string]string
+	orbitedBy map[string][]string // part2
+	visited   map[string]struct{}
 )
 
 func main() {
@@ -18,12 +21,10 @@ func main() {
 		log.Error(err.Error())
 		os.Exit(1)
 	}
-
 	contents := string(input)
-
 	//	contents := `COM)B
-	//B)C
-	//C)D
+	//B)YOU
+	//YOU)D
 	//D)E
 	//E)F
 	//B)G
@@ -31,8 +32,11 @@ func main() {
 	//D)I
 	//E)J
 	//J)K
-	//K)L`
+	//K)L
+	//I)SAN`
 	orbits = make(map[string]string)
+	orbitedBy = make(map[string][]string)
+	visited = make(map[string]struct{})
 
 	in := strings.Split(contents, "\n")
 	for _, o := range in {
@@ -40,26 +44,35 @@ func main() {
 		orbitee := objects[0] // the object orbited by some other object A)B => A
 		orbiter := objects[1] // the object orbiting some other object A)B => B
 		orbits[orbiter] = orbitee
+
+		orbitedBy[orbitee] = append(orbitedBy[orbitee], orbiter)
 	}
 
 	totalOrbits := 0
 	// iterate over all objects that orbit another object
 	for orbiter := range orbits {
 		dist := distToCOM(orbiter)
-		fmt.Printf("Dist of %s to COM: %d\n", orbiter, dist)
+		//fmt.Printf("Dist of %s to COM: %d\n", orbiter, dist)
 		totalOrbits += dist
 	}
 	fmt.Printf("Total orbits: %d\n", totalOrbits)
+
+	dist := walk("YOU", "SAN", 0)
+	fmt.Printf("Dist to SAN: %d", dist-2)
 }
 
 func distToCOM(orbiter string) int {
+	return distTo("COM", orbiter)
+}
+
+func distTo(obj, orbiter string) int {
 	// will not happen because every object orbits exactly one other object according to problem statement
 	//if _, ok := orbitMap[orbiter]; !ok {
 	//	return -1
 	//}
 	dist := 0
 	currObj := orbiter
-	for currObj != "COM" {
+	for currObj != obj {
 		orbitee, ok := orbits[currObj]
 		if !ok {
 			return 0
@@ -69,3 +82,46 @@ func distToCOM(orbiter string) int {
 	}
 	return dist
 }
+
+func walk(curr, end string, currLengthFromStart int) int {
+	if _, ok := visited[curr]; ok {
+		return math.MaxInt32
+	}
+
+	if curr == end {
+		return currLengthFromStart
+	}
+
+	// mark planet as visited
+	visited[curr] = struct{}{}
+
+	// walk through all objects that orbit curr
+	distFromOrbiters := math.MaxInt32
+	distFromOrbitees := math.MaxInt32
+	if orbiters, hasOrbiters := orbitedBy[curr]; hasOrbiters {
+		for _, orbiter := range orbiters {
+			tmp := walk(orbiter, end, currLengthFromStart+1)
+			distFromOrbiters = min(distFromOrbiters, tmp)
+		}
+	}
+	// go to orbitee
+	if orbitee, hasOrbitee := orbits[curr]; hasOrbitee {
+		tmp := walk(orbitee, end, currLengthFromStart+1)
+		distFromOrbitees = min(distFromOrbitees, tmp)
+	}
+	return min(distFromOrbitees, distFromOrbiters)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//func search(curr, end string) int {
+//	if curr == end {
+//		return 0
+//	}
+//	return 1 + search()
+//}
