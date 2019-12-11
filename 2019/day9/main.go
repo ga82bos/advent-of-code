03ebe6c4a21376f5d478fa3e1e27ddefa9ce26e9
ga82bos <max.zitzmann@tum.de>
2019-12-11 15:30:20 +0100

    Solution to day 9

--- 2019/day9/main.go
+++ 2019/day9/main.go
@@ -13,16 +13,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	//mem := "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
-	//mem := "1102,34915192,34915192,7,4,7,99,0"
-	//mem :="104,1125899906842624,99"
+	res := Day9Task(mem, 1)
+	shared.PrintSolution(9, 1, "BOOST keycode: %d", res)
+	res = Day9Task(mem, 2)
+	shared.PrintSolution(9, 2, "coordinates of the distress signal: %d", res)
+}
+
+func Day9Task(mem string, in int) int {
 	c := shared.NewComputer(0, mem, func(cid int, err error) {
 		fmt.Printf("Error: %v", err)
 		os.Exit(1)
 	})
-	c.QueueInput(2)
+	c.QueueInput(in)
 	c.Run()
 	c.WaitUntilHalted()
 	outputs, _ := c.ReadAllOutputs()
-	fmt.Printf("%v\n", outputs)
+	return outputs[0]
 }
